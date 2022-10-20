e60c24b73ec62d4f0f38e9e5be2ec5943420034b
Valerii0208 <valerii.kryvoruchko.02@gmail.com>
2022-10-21 00:51:35 +0300

    add my gradle project

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    String test;
+}
---
+++ src/main/java/TestMain.java
@@ -0,0 +1,7 @@
+public class TestMain {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.setTest("Hello");
+        System.out.println(test.getTest());
+    }
+}
